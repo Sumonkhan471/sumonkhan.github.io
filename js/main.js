7d0bfe51aa1def7280284820b8efcf337c05052f
Sumonkhan471 <khansu471@gmail.com>
2018-06-26 08:01:24 +0600

    my first web site
    my web fotfulio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+	
+	
+	$(window).load(function() {
+	$('#visual').pignoseParallaxSlider({
+		play    : '.btn-play',
+		pause   : '.btn-pause',
+		next    : '.btn-next',
+		prev    : '.btn-prev'
+		});
+	});
+	
+	
+	
+	
+	$('.image-link').viewbox();
+	
+	$(".mymenu").tolTip
+	
+});
+
